1d08c1bf20eb6351a946b9b3761f31c76d2242a0
Timo Korinth <info@timokorinth.de>
2023-12-01 11:41:43 +0100

    update free wingman

--- wingmen/free_wingman.py
+++ wingmen/free_wingman.py
@@ -5,11 +5,15 @@ import speech_recognition as sr
 
 
 class FreeWingman(Wingman):
+    def __init__(self, name: str, config: dict[str, any]):
+        super().__init__(name, config)
+        self.edge_tts = EdgeTTS()
+        self.r = sr.Recognizer()
+
     async def _transcribe(self, audio_input_wav: str) -> tuple[str | None, str | None]:
-        r = sr.Recognizer()
         with sr.AudioFile(audio_input_wav) as source:
-            audio = r.record(source)
-        result = r.recognize_google(audio)
+            audio = self.r.record(source)
+        result = self.r.recognize_google(audio)
 
         return result, None
 
@@ -25,9 +29,16 @@ class FreeWingman(Wingman):
     async def _play_to_user(self, text: str):
         if not text or text == "None":
             return
-        edge_tts = EdgeTTS()
-        random_voice = await edge_tts.get_random_voice()
-        await edge_tts.generate_speech(
-            text, filename="audio_output/free.mp3", voice=random_voice
-        )
-        self.audio_player.play("audio_output/free.mp3")
+
+        if self.tts_provider == "edge_tts":
+            edge_config = self.config["edge_tts"]
+            tts_voice = edge_config.get("tts_voice")
+
+            await self.edge_tts.generate_speech(
+                text, filename="audio_output/free.mp3", voice=tts_voice
+            )
+
+            if self.config.get("features", {}).get("enable_robot_sound_effect"):
+                self.audio_player.effect_audio("audio_output/free.mp3")
+
+            self.audio_player.play("audio_output/free.mp3")
